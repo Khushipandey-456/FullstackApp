7ce6c03179cf9eaaafca20baa80adc89af793150
Khushipandey-456 <kp9761560@gmail.com>
2024-12-26 13:48:24 +0530

    Navbardone

---
+++ scripts/navbar.js
@@ -0,0 +1,15 @@
+let navbar=()=>{
+    let card =`<div id= "nav-container">
+    <a id="logo" href="">My Personal App</a>
+     <div id="navlinks>
+    <a href="#"></a>
+     <a href="signup.html">SignUp</a>
+     <a href="login.html">Login</a>
+     <a href="todos.html">Todos</a>
+      <a href="expences.html">Expences</a>
+      </div>
+    </div>`
+    document.getElementById("nav").innerHTML=card;
+
+};
+navbar()
